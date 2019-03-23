226b61a149974dbc1e329266c808161383357b4c
bigbeefbaby <46167348+bigbeefbaby@users.noreply.github.com>
2019-03-23 15:51:50 +0000

    Added new patches thx to lalitjoshi06

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -24,8 +24,44 @@ for var in eng user userdebug; do
   add_lunch_combo lineage_mix-$var
 done
 
-# Auto Patches-removed
+
 
 
 #Ship with root access
 export WITH_SU=true
+
+#!/bin/bash
+cd ../../../..
+cd system/core
+git apply -v ../../device/doogee/mix/patches_mtk/system_core.patch
+cd ../..
+cd bionic
+git apply -v ../device/doogee/mix/patches_mtk/bionic.patch
+cd ..
+cd system/sepolicy
+git apply -v ../../device/doogee/mix/patches_mtk/system_sepolicy.patch
+cd ../..
+cd frameworks/av
+git apply -v ../../device/doogee/mix/patches_mtk/frameworks_av.patch
+cd ..
+cd native
+git apply -v ../../device/doogee/mix/patches_mtk/frameworks_native.patch
+cd ..
+cd base
+git apply -v ../../device/doogee/mix/patches_mtk/frameworks_base.patch
+cd ..
+cd opt/telephony
+git apply -v ../../../device/doogee/mix/patches_mtk/frameworks_opt_telephony.patch
+cd ../../..
+cd packages/apps/Snap
+git apply -v ../../../device/doogee/mix/patches_mtk/snap.patch
+cd ..
+cd FMRadio
+git apply -v ../../../device/doogee/mix/patches_mtk/fmradio.patch
+cd ../../..
+cd system/netd
+git apply -v ../../device/doogee/mix/patches_mtk/system_netd.patch
+cd ../..
+cd vendor/cmsdk
+git apply -v ../../device/doogee/mix/patches_mtk/vendor_cmsdk.patch
+cd ../..
