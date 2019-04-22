ed5b9e5c58880abd3f46034436048e77e172860d
bigbeefbaby <46167348+bigbeefbaby@users.noreply.github.com>
2019-04-22 17:17:29 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,9 +20,8 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-for var in eng user userdebug; do
-  add_lunch_combo lineage_mix-$var
-done
+add_lunch_combo lineage_mix-userdebug
+add_lunch_combo lineage_mix-user
 
 #!/bin/bash
 
