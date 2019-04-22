de29dfa8586197234b8785e22f16ec4ae241ccd3
bigbeefbaby <46167348+bigbeefbaby@users.noreply.github.com>
2019-04-22 18:17:09 +0100

    Changed file name

--- lineage_mix.mk
+++ lineage_mix.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, $(LOCAL_PATH)/device.mk)
+$(call inherit-product, $(LOCAL_PATH)/device_mix.mk)
 
 # Device display
 TARGET_SCREEN_HEIGHT := 1080
